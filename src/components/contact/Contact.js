3c3ebeee40364488878ad68b49336bb697e8205c
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-09 20:40:34 +0100

    Create contact component

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,72 @@
+import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+import { useRef, useState } from "react";
+import emailjs from "emailjs-com";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_k5tapij",
+        "template_8aun808",
+        formRef.current,
+        "EpTajGY-J62tQcGmk"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +47 926 24 792
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Email} alt="" />
+              thomasstorbakk@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="" />
+              Oslo, Norway
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What's your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along. me.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea rows="5" placeholder="Message" name="message" />
+            <button>Submit</button>
+            {done && <p className="c-thanks">"Thank you! I'll get back to you shortly!"</p>}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
